61cdc719cb0fee731e7b6e66b862e3753bbc43a9
Hao Chen <hche8093@uni.sydney.edu.au>
2023-02-08 17:10:06 +1100

    Resolve nested path

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -15,7 +15,8 @@ export const unpkgPathPlugin = () => {
         if (args.path.includes('./') || args.path.includes('../')) {
           return {
             namespace: 'a',
-            path: new URL(args.path, args.importer + '/').href,
+            path: new URL(args.path, `https://unpkg.com${args.resolveDir}/`)
+              .href,
           };
         }
 
@@ -39,17 +40,21 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('medium-test-pkg');
+              const message = require('nested-test-pkg');
               console.log(message);
             `,
           };
         }
 
-        const { data } = await axios.get(args.path);
+        const { data, request } = await axios.get(args.path);
+
+        // console.log(request);
 
         return {
           loader: 'jsx',
           contents: data,
+          // Resolve nested path
+          resolveDir: new URL('./', request.responseURL).pathname,
         };
       });
     },
