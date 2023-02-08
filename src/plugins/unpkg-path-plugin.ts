18287b2b3c7b2ead7a462be234fff1a01f0cc7cc
Hao Chen <hche8093@uni.sydney.edu.au>
2023-02-09 10:49:37 +1100

    Add bundle user input

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -6,7 +6,7 @@ const fileCache = localforage.createInstance({
   name: 'filecache',
 });
 
-export const unpkgPathPlugin = () => {
+export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
@@ -44,11 +44,7 @@ export const unpkgPathPlugin = () => {
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
-            contents: `
-              const react = require('react');
-              const reactDOM = require('react-dom');
-              console.log(react, reactDOM);
-            `,
+            contents: inputCode,
           };
         }
 
