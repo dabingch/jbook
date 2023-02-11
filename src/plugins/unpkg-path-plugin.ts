51230199625e45a058cd7195454f86df98c8f9f6
Hao Chen <hche8093@uni.sydney.edu.au>
2023-02-12 10:42:11 +1100

    Breaking up resolve logic with filters

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -10,32 +10,25 @@ export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
+      // Handle root entry file of index.js
+      build.onResolve({ filter: /^index\.js$/ }, () => {
+        return { path: 'index.js', namespace: 'a' };
+      });
+
+      // Handle relative path in a module
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
+        return {
+          namespace: 'a',
+          path: new URL(args.path, `https://unpkg.com${args.resolveDir}/`).href,
+        };
+      });
+
+      // Handle main file of a module
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResolve', args);
-        if (args.path === 'index.js') {
-          return { path: args.path, namespace: 'a' };
-        }
-
-        // Solve relative path
-        if (args.path.includes('./') || args.path.includes('../')) {
-          return {
-            namespace: 'a',
-            path: new URL(args.path, `https://unpkg.com${args.resolveDir}/`)
-              .href,
-          };
-        }
-
         return {
           path: `https://unpkg.com/${args.path}`,
           namespace: 'a',
         };
-
-        // else if (args.path === 'tiny-test-pkg') {
-        //   return {
-        //     namespace: 'a',
-        //     path: `https://unpkg.com/tiny-test-pkg@1.0.0/index.js`,
-        //   };
-        // }
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
